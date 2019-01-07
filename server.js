ef29d9cd2a954bc0bd0d0e9095fb20a007228257
Glitch (jagged-sweater) <none>
2019-01-07 08:29:32 +0000

    🌟💉 Checkpoint ./server.js:2689143/325 ./package.json:2689143/1054

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 
+var useragent = require('express-useragent');
+
+
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -12,6 +16,8 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+app.use(useragent.express()); 
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -26,9 +32,13 @@ app.get("/api/hello", function (req, res) {
 
 app.get(api, (req, res, next) => {
   var language = req.acceptsLanguages();
-  var software;
+  var software = 
+      "OS" + req.useragent.os + 
+      ", Browser: " + req.useragent.browser;
+  // var software = req.get('User-Agent');
+  //same way: req.headers['user-agent'];
   var ipadress = req.ip;
-  res.json({ipadress : ipadress, language: language[]});
+  res.json({ipadress : ipadress, language: language[0], software: software});
 });
 
 // listen for requests :)
